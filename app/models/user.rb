38c1eea0b58230f6f6f59f093c9338cca352a58c
Binish Khan <binishbaig13@gmail.com>
2014-10-09 08:41:27 -0500

    loop error

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,32 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                     :integer          not null, primary key
+#  email                  :string(255)      default(""), not null
+#  encrypted_password     :string(255)      default(""), not null
+#  reset_password_token   :string(255)
+#  reset_password_sent_at :datetime
+#  remember_created_at    :datetime
+#  sign_in_count          :integer          default(0), not null
+#  current_sign_in_at     :datetime
+#  last_sign_in_at        :datetime
+#  current_sign_in_ip     :string(255)
+#  last_sign_in_ip        :string(255)
+#  created_at             :datetime
+#  updated_at             :datetime
+#  role                   :string(255)
+#  username               :string(255)
+#
+
 class User < ActiveRecord::Base
+
+	before_create :set_default_role
+
+	def set_default_role
+		self.role = 'admin' unless role==""
+	end
+
 
 	validates :username, presence: true, length: {maximum: 255}, uniqueness: { case_sensitive: false }, format: { with: /\A[a-zA-Z0-9]*\z/, message: "may only contain letters and numbers." }
   # Include default devise modules. Others available are:
