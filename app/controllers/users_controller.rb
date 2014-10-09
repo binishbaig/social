e244fec69c00fcc2e60c1a6aaa92b56069f6251f
Binish Khan <binishbaig13@gmail.com>
2014-10-09 10:31:09 -0500

    fixed login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,12 @@
 class UsersController < ApplicationController
-
+  before_filter :authenticate_user!
 
 	def index
 		@users = User.all
+    @admin = User.first
+  unless @admin == current_user
+    redirect_to :back, :alert => "Access denied."
+  end
 		
 	end
 
